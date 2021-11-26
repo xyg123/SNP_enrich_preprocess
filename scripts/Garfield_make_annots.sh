4813816cb83d0c784ebf1b8c30282422a5f45536
xyg123 <xiangyuge123@hotmail.co.uk>
2021-11-26 11:01:17 +0000

    Performed cross comparison of methods

--- scripts/Garfield_make_annots.sh
+++ scripts/Garfield_make_annots.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 
-INPUT_SUMSTATS=$1
-TRAIT_NAME=$2
+INPUT_ANNOTATIONS=$1
+OUTPUT_ANNOTATIONS=$2
 DATA_DIR=$3
 
 
@@ -13,11 +13,25 @@ DATA_DIR=$3
 
 # Reformat and concat into garfield format:
 
-for i in {1..22}; do awk '{ print $1 }' ~/Downloads/garfield-data/maftssd/chr${i} > tmp.chr${i};  for FILE in ~/Downloads/garfield-data/test/chr${i}/* ; do paste tmp.chr${i} <(awk '{ print $2}' <(tail -n +2 ${FILE})) > tmp0.chr${i} ; cp tmp0.chr${i} tmp.chr${i} ; done; done
+for i in {1..22} "X"; do 
+    awk '{ print $1 }' ~/Downloads/garfield-data/maftssd/chr${i} > tmp.chr${i};
+      for FILE in ~/Downloads/garfield-data/Epimap_EP_annots_new/chr${i}/* ; do 
+        paste tmp.chr${i} <(awk '{ print $2}' <(tail -n +2 ${FILE})) > tmp0.chr${i} ;
+        cp tmp0.chr${i} tmp.chr${i} ;
+        done; 
+    done
 
-for f in {1..22}; do cp  chr${f} chr${f}.tmp; paste -d" " <(awk '{print $1}' chr${f}.tmp | sed 1d) <(awk '{$1=""; print $0}' chr${f}.tmp | awk '{ gsub("\t",""); print;}' | awk '{ gsub(" ",""); print;}'| sed 1d) > chr${f}.test; done
+for f in {1..22} "X"; do 
+  paste -d" " <(awk '{print $1}' tmp.chr${f} | sed 1d) <(awk '{$1=""; print $0}' tmp.chr${f} | awk '{ gsub("\t",""); print;}' | awk '{ gsub(" ",""); print;}'| sed 1d) > chr${f}; 
+done
+
+for f in {1..22} "X"; do 
+  tail -n +2 chr${f}/BSS00196 > tmp0.chr${i} ;
+  #cp /Users/xg1/Downloads/garfield-data/Epimap_EP_annots/chr${f}/BSS00196 /Users/xg1/Downloads/garfield-data/Epimap_EP_annots/chr${f}/tmp.chr${f};
+  paste -d" " <(awk '{print $1}' tmp0.chr${i}   | sed 1d) <(awk '{$1=""; print $0}' tmp0.chr${i}  | awk '{ gsub("\t",""); print;}' | awk '{ gsub(" ",""); print;}'| sed 1d) > chr${f}.full; 
+done
 
 
- for FILE in ~/Downloads/garfield-data/test_concat/* ; do paste tmp.chrX <(awk '{ print $2}' <(tail -n +2 ${FILE})) > tmp0.chrX ; cp tmp0.chrX tmp.chrX ; done
+for FILE in ~/Downloads/garfield-data/test_concat/* ; do paste tmp.chrX <(awk '{ print $2}' <(tail -n +2 ${FILE})) > tmp0.chrX ; cp tmp0.chrX tmp.chrX ; done
 
 paste -d" " <(awk '{print $1}' tmp.chrX | sed 1d) <(awk '{$1=""; print $0}' tmp.chrX | awk '{ gsub("\t",""); print;}' | awk '{ gsub(" ",""); print;}'| sed 1d) > chrX.test
