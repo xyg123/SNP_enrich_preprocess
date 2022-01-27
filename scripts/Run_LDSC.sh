c1bbeee43abf8791659a9a525f5dadb4ee33141e
xyg123 <xiangyuge123@hotmail.co.uk>
2022-01-27 09:34:14 +0000

    preprocessing sumstats  with BQ and auto computation of LDSC

--- scripts/Run_LDSC.sh
+++ scripts/Run_LDSC.sh
@@ -10,11 +10,12 @@ gsutil cp gs://genetics-portal-dev-analysis/xg1/rsid_sumstats/${Input_study} ../
 
 conda activate ldsc
 
-python munge_sumstats.py --sumstats ../tmp/${Input_study} --merge-alleles ${LDSC_dir}/w_hm3.snplist --out ${LDSC_dir}/munged_sumstats/${Input_study} --a1-inc
+python ${LDSC_dir}/munge_sumstats.py --sumstats ../tmp/${Input_study} --merge-alleles ${LDSC_dir}/w_hm3.snplist --out ${LDSC_dir}/munged_sumstats/${Input_study} --a1-inc
 rm ../tmp/${Input_study}
 
 #for FILE in ${Annot_dir}/*; do python ldsc.py --h2 ${LDSC_dir}/munged_sumstats/${Input_study} --ref-ld-chr ${LDSC_dir}/baselineLD/baselineLD.,${LDSC_dir}/Comparison_EP_concat/Epimap_EP. --frqfile-chr ${LDSC_dir}/1000G_Phase3_frq/1000G.EUR.QC. --w-ld-chr ${LDSC_dir}/weights_hm3_no_hla/weights. --overlap-annot --print-coefficients --print-delete-vals --out ${LDSC_dir}/results/${Input_study}; done
-for PART in {1..17}; do python ldsc.py --h2 ${LDSC_dir}/munged_sumstats/${Input_study} --ref-ld-chr ${LDSC_dir}/baselineLD/baselineLD.,${Annot_dir}/part_${PART}/Epimap. --frqfile-chr ${LDSC_dir}/1000G_Phase3_frq/1000G.EUR.QC. --w-ld-chr ${LDSC_dir}/weights_hm3_no_hla/weights. --overlap-annot --print-coefficients --print-delete-vals --out ${LDSC_dir}/results/${Input_study}; done
+for PART in {1..17}; do python ${LDSC_dir}/ldsc.py --h2 ${LDSC_dir}/munged_sumstats/${Input_study} --ref-ld-chr ${LDSC_dir}/baselineLD/baselineLD.,${Annot_dir}/part_${PART}/Epimap. --frqfile-chr ${LDSC_dir}/1000G_Phase3_frq/1000G.EUR.QC. --w-ld-chr ${LDSC_dir}/weights_hm3_no_hla/weights. --overlap-annot --print-coefficients --print-delete-vals --out ${LDSC_dir}/results/${Input_study}; done
 
 # Run script to merge results:
 
+#bash Run_LDSC.sh GCST002245 ~/ldsc  ~/LDSC_full_epimap_parts
