1434448fa45d8ba0300bf652d5c1defa1f4fd7c2
xyg123 <33658607+xyg123@users.noreply.github.com>
2022-02-02 21:30:40 +0000

    Update Prep_BQ_sumstats.sh
    reformatted sumstat bash script

--- scripts/Prep_BQ_sumstats.sh
+++ scripts/Prep_BQ_sumstats.sh
@@ -1,26 +1,23 @@
 #!/usr/bin/env bash
 
 Completed_enrichments=$1
-## gs://genetics-portal-dev-analysis/xg1/LDSC_enrichments
+## gs://genetics-portal-dev-analysis/xg1/LDSC_enrichments/*
 Available_inputs=$2
+## gs://genetics-portal-dev-analysis/xg1/Test_sumstat_inputs/*.parquet
+
+NCORES=$3
 
 gsutil -m ls -d $Completed_enrichments > existing_enrichments.txt
 gsutil -m ls -d $Available_inputs > input_sumstats.txt
+
+# writes the formatting commands to "format_commands.txt":
 
 python scripts/Generate_manifest.py --in_existing existing_enrichments.txt --in_sumstats input_sumstats.txt
 
 
 
-#zcat Format_sumstats_commands | shuf | parallel -j $NCORES --joblog logs/parallel.jobs.log
+# pipe formatting sumstat commands to parallel:
 
+cat format_commands.txt | parallel -j $NCORES --joblog logs/parallel.jobs.log
 
-
-
-# NCORES=$1
-
-# set -euo pipefail
-
-# python 3_make_commands.py | shuf | parallel -j $NCORES --bar --joblog logs/parallel.jobs.log
-
-# echo COMPLETE
 
